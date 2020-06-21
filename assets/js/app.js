0c90f0e6b9cd6310128b0480dc5370ac3a0a6b3d
Guuri11 <sergio.gurillo11@gmail.com>
2020-06-21 13:00:04 +0200

    heroku commit

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,14 +1,16 @@
-/*
- * Welcome to your app's main JavaScript file!
- *
- * We recommend including the built version of this JavaScript file
- * (and its CSS file) in your base layout (base.html.twig).
- */
-
-// any CSS you import will output into a single css file (app.css in this case)
+import React from 'react';
+import ReactDOM from 'react-dom';
+import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
 import '../css/app.css';
 
-// Need jQuery? Install it with "yarn add jquery", then uncomment to import it.
-// import $ from 'jquery';
 
-console.log('Hello Webpack Encore! Edit me in assets/js/app.js');
+ReactDOM.render(
+    /*<Router>
+        <Switch>
+            <Route exact path="/artistas/:artist" component={Artist}/>
+        </Switch>
+    </Router>*/
+    <div>
+        <h1>Hola SoftProd</h1>
+    </div>
+    ,document.getElementById('root'));
